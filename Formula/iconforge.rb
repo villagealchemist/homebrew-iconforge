e385a3369b0d3cf6313c18b7152372a0522cabdf
villagealchemist <mjohnson1307@gmail.com>
2025-05-27 08:44:53 -0400

    onee moreee time

--- Formula/iconforge.rb
+++ Formula/iconforge.rb
@@ -2,7 +2,7 @@ class Iconforge < Formula
   desc "Convert any image into a macOS .icns icon bundle from the terminal"
   homepage "https://github.com/villagealchemist/iconforge"
   url "https://github.com/villagealchemist/iconforge/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "1118dc86c1d7d58043275ff16ae947210c9083cc9710b63530ae2dc8a1c48787"
+  sha256 "c3017ff6bb2d786b30b69f5b6a0a4d0e9d04af6ffc561f143546b5ded3ac5f18"
   license "MIT"
   version "1.0.0"
 
